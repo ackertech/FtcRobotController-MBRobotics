0cfe693e2068fbd912d09db2bc52d44d71f12c1c
AckerTech <pushkinvol@gmail.com>
2024-10-01 07:35:55 -0400

    Testing First Push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Robot/MechBot.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Base.Robot;
+
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.Base.Drivetrains.MecanumDrive;
+
+
+public class MechBot extends MecanumDrive {
+
+    // Robot Hardware Constructors
+    public HardwareMap hwBot  =  null;
+
+    // Declaration of Timer Variables
+    public ElapsedTime currentTime = new ElapsedTime();
+
+    public ElapsedTime timer = new ElapsedTime();
+    double waitTime = 2.0;
+
+    // Declaration of Gyro Variables
+    RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+    RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+
+    //MechBot Constructor
+    public MechBot() {}
+
+    // Initializing Hardware for Robot
+    public void initRobot(HardwareMap hwMap) {
+
+        hwBot = hwMap;
+
+        // Define Motors for Robot
+        frontLeftMotor = hwBot.dcMotor.get("front_left_motor");
+        frontRightMotor = hwBot.dcMotor.get("front_right_motor");
+        rearLeftMotor = hwBot.dcMotor.get("rear_left_motor");
+        rearRightMotor = hwBot.dcMotor.get("rear_right_motor");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+
+        //Initialize Motor Run Mode for Robot
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Timer Reset
+        currentTime.reset();
+
+        // Gyro Initiatlization
+        imu = hwBot.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+
+    }
+
+}
