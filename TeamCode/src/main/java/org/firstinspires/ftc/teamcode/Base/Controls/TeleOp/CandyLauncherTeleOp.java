9624e298f1a989dcb11387acf8b5b4c7ce01e1c2
AckerTech <pushkinvol@gmail.com>
2025-02-03 09:30:40 -0500

    CandyBot Code for Robots

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/CandyLauncherTeleOp.java
@@ -0,0 +1,251 @@
+package org.firstinspires.ftc.teamcode.Base.Controls.TeleOp;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Base.Robot.CandyBot;
+import org.firstinspires.ftc.teamcode.Base.Robot.TankBot;
+
+//@Disabled
+@TeleOp(name = "Candy Launcher",group="iLab")
+
+public class CandyLauncherTeleOp extends OpMode {
+
+    //TeleOp Driving Behavior Variables
+    public double speedMultiply = 1;
+    public enum Style {
+        ARCADE1, ARCADE2, TANK
+    }
+
+    public Style driverStyle = Style.ARCADE1;
+    public double leftSidePower;
+    public double rightSidePower;
+
+    // GamePad Variables
+    public float leftStickY1;
+    public float rightStickY1;
+    public float leftStickX1;
+    public float rightStickX1;
+
+    public double leftMotorValue;
+    public double rightMotorValue;
+
+    // Construct the Physical Bot based on the Robot Class
+    public CandyBot Bot = new CandyBot();
+
+
+    // TeleOp Initialize Method.  This is the Init Button on the Driver Station Phone
+    @Override
+    public void init() {
+
+        Bot.initDrive(hardwareMap);
+        Bot.initFlyWheels(hardwareMap);
+        Bot.initLinearActuator(hardwareMap);
+        Bot.initServo1(hardwareMap);
+        Bot.initServo2(hardwareMap);
+        Bot.initServo3(hardwareMap);
+
+        leftStickY1 = 0;
+        leftStickX1 = 0;
+        rightStickY1 = 0;
+        rightStickX1 = 0;
+
+    }
+
+    // TeleOp Loop Method.  This start AFTER clicking the Play Button on the Driver Station Phone
+
+    public void loop() {
+
+        //Drive Comtroller Methods
+        getController();
+        speedControl();
+        driveControl();
+
+        //Mechanism Comtroller Methods
+        flyWheelControl();
+        linearActuatorControl();
+        wormGearControl();
+        servoOneControl();
+        servoTwoControl();
+        servoThreeControl();
+
+        //Telemetry Controller
+        telemetryOutput();
+
+    }
+
+    /**  ********  TELEMETRY OUTPUT *************      **/
+
+    public void telemetryOutput() {
+        telemetry.addData("Drive Mode: ", driverStyle);
+        telemetry.addData("Speed: ", speedMultiply);
+        telemetry.addData("Front Left Motor Power: ", Bot.frontLeftMotor.getPower());
+        telemetry.addData("Rear Left Motor Power: ", Bot.rearLeftMotor.getPower());
+        telemetry.addData("Front Right Motor Power: ", Bot.frontRightMotor.getPower());
+        telemetry.addData("Rear Right Motor Power: ", Bot.rearRightMotor.getPower());
+        telemetry.addData("Fly Wheel 1: ", Bot.flyWheel1.getPower());
+        telemetry.addData("Fly Wheel 2: ", Bot.flyWheel2.getPower());
+        telemetry.addData("Worm Gear: ", Bot.wormGear.getPower());
+        telemetry.addData("Linear Actuator: ", Bot.linearActuator.getPower());
+        telemetry.addData("Servo 1: ", Bot.servo1.getPosition());
+        telemetry.addData("Servo 2: ", Bot.servo2.getPosition());
+        telemetry.addData("Servo 3: ", Bot.servo3.getPosition());
+        telemetry.update();
+
+    }
+
+    /**  ********  DRIVING METHODS USING GAMEPAD 1 *************      **/
+
+    public void getController() {
+        leftStickY1 = gamepad1.left_stick_y;
+        leftStickX1 = gamepad1.left_stick_x;
+        rightStickY1 = gamepad1.right_stick_y;
+        rightStickX1 = gamepad1.right_stick_x;
+    }
+
+    public void driveControl() {
+
+        if (gamepad1.a) {
+            driverStyle = Style.ARCADE1;
+        }
+        if (gamepad1.b) {
+            driverStyle = Style.ARCADE2;
+        }
+        if (gamepad1.y) {
+            driverStyle = Style.TANK;
+        }
+
+        switch (driverStyle) {
+
+            case ARCADE1:
+
+                leftMotorValue = leftStickY1 - leftStickX1;
+                rightMotorValue = leftStickY1 + leftStickX1;
+                leftMotorValue = Range.clip(leftMotorValue, -1, 1);
+                rightMotorValue = Range.clip(rightMotorValue, -1, 1);
+                Bot.frontLeftMotor.setPower(leftMotorValue * speedMultiply);
+                Bot.rearLeftMotor.setPower(leftMotorValue * speedMultiply);
+                Bot.frontRightMotor.setPower(rightMotorValue * speedMultiply);
+                Bot.rearRightMotor.setPower(rightMotorValue * speedMultiply);
+                break;
+
+            case ARCADE2:
+                leftMotorValue = leftStickY1 - rightStickX1;
+                rightMotorValue = leftStickY1 + rightStickX1;
+                leftMotorValue = Range.clip(leftMotorValue, -1, 1);
+                rightMotorValue = Range.clip(rightMotorValue, -1, 1);
+                Bot.frontLeftMotor.setPower(leftMotorValue * speedMultiply);
+                Bot.rearLeftMotor.setPower(leftMotorValue * speedMultiply);
+                Bot.frontRightMotor.setPower(rightMotorValue * speedMultiply);
+                Bot.rearRightMotor.setPower(rightMotorValue * speedMultiply);
+                break;
+
+            case TANK:
+
+                double powerFLM = leftStickY1 * speedMultiply;
+                double powerRLM = leftStickY1 * speedMultiply;
+                double powerFRM = rightStickY1 * speedMultiply;
+                double powerRRM = rightStickY1 * speedMultiply;
+
+                Bot.frontLeftMotor.setPower(powerFLM);
+                Bot.rearLeftMotor.setPower(powerRLM);
+                Bot.frontRightMotor.setPower(powerFRM);
+                Bot.rearRightMotor.setPower(powerRRM);
+
+                break;
+        }
+    }
+
+    public void speedControl () {
+            if (gamepad1.dpad_right) {
+                speedMultiply = 0.25;
+            } else if (gamepad1.dpad_right) {
+                speedMultiply = 0.50;
+            } else if (gamepad1.dpad_down) {
+                speedMultiply = 0.75;
+            } else if (gamepad1.dpad_left) {
+                speedMultiply = 1.00;
+            }
+    }
+
+    /**  ********  DRIVING METHODS USING GAMEPAD 2 *************      **/
+
+
+    public void flyWheelControl()
+    {
+        if (gamepad2.left_bumper) {
+            Bot.rotateFlyWheel1(1.0);
+            Bot.rotateFlyWheel2(-1.0);
+        }
+
+        if (gamepad2.right_bumper) {
+            Bot.stopFlyWheel1();
+            Bot.stopFlyWheel2();
+        }
+
+    }
+
+    public void wormGearControl()
+    {
+        if (gamepad2.left_trigger > 0.1) {
+            Bot.wormGearRotateForward(0.90);
+        }
+        else if (gamepad2.right_trigger > 0.1) {
+            Bot.wormGearRotateReverse(-0.90);
+        }
+        else
+        {
+            Bot.wormGearStop();
+        }
+    }
+
+    public void linearActuatorControl() {
+        if (gamepad2.dpad_up) {
+            Bot.extendLinear(.90);
+        }
+        else if (gamepad2.dpad_up) {
+            Bot.retractLinear(90);
+        }
+        else
+        {
+            Bot.stopLinear();
+        }
+
+    }
+
+    public void servoOneControl() {
+        if (gamepad2.y) {
+            Bot.extendServo1();
+        }
+
+        if (gamepad2.x) {
+            Bot.retractServo1();
+        }
+    }
+
+    public void servoTwoControl() {
+        if (gamepad2.a) {
+            Bot.extendServo2();
+        }
+
+        if (gamepad2.b) {
+            Bot.retractServo2();
+        }
+    }
+
+    public void servoThreeControl() {
+        if (gamepad2.dpad_left) {
+            Bot.extendServo3();
+        }
+
+        if (gamepad2.dpad_right) {
+            Bot.retractServo3();
+        }
+    }
+
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Robot/CandyBot.java
@@ -0,0 +1,185 @@
+package org.firstinspires.ftc.teamcode.Base.Robot;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Base.Drivetrains.Carnival_Drive;
+
+public class CandyBot extends Carnival_Drive {
+
+    public HardwareMap hwBot = null;
+
+    // Motors
+    public DcMotor wormGear;
+    public DcMotor linearActuator;
+    public DcMotor flyWheel1;
+    public DcMotor flyWheel2;
+
+
+    // Servos
+    public Servo servo1;
+    public Servo servo2;
+    public Servo servo3;
+
+    public CandyBot() {}
+
+    // **** Initialize Drivetrain Hardware ****
+    public void initDrive(HardwareMap hwMap) {
+        hwBot = hwMap;
+
+        // Drive Motors
+        frontLeftMotor = hwBot.dcMotor.get("front_left_motor");     // Control Hub Port 0
+        frontRightMotor = hwBot.dcMotor.get("front_right_motor");   // Control Hub Port 2
+        rearLeftMotor = hwBot.dcMotor.get("rear_left_motor");       // Control Hub Port 1
+        rearRightMotor = hwBot.dcMotor.get("rear_right_motor");     // Control Hub Port 3
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+    }
+
+    // **** Initialize Fly Wheel Hardware ****
+    public void initFlyWheels(HardwareMap hwMap) {
+        hwBot = hwMap;
+        flyWheel1 = hwBot.dcMotor.get("fly_wheel1");                        //Expansion Port 0
+        flyWheel1.setDirection(DcMotorSimple.Direction.FORWARD);
+        flyWheel1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        flyWheel2 = hwBot.dcMotor.get("fly_wheel2");                        //Expansion Port 1
+        flyWheel2.setDirection(DcMotorSimple.Direction.FORWARD);
+        flyWheel2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+    // **** Initialize Worm Gear Mechanism ****
+    public void initWormGear(HardwareMap hwMap) {
+        hwBot = hwMap;
+        wormGear = hwBot.dcMotor.get("worm_gear");                          //Expansion Port 2
+        wormGear.setDirection(DcMotorSimple.Direction.FORWARD);
+        wormGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    // **** Initialize Linear Actuator  ****
+    public void initLinearActuator(HardwareMap hwMap) {
+        hwBot = hwMap;
+        linearActuator = hwBot.dcMotor.get("linear_actuator");             //Expansion Port 3
+        linearActuator.setDirection(DcMotorSimple.Direction.FORWARD);
+        linearActuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearActuator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        linearActuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    // **** Initialize Servo  Hardware ****
+    public void initServo1(HardwareMap hwMap) {
+        hwBot = hwMap;
+        servo1 = hwBot.get(Servo.class, "servo1");          //Control Hub Servo Port 0
+        servo1.setDirection(Servo.Direction.FORWARD);
+    }
+
+    public void initServo2(HardwareMap hwMap) {
+        hwBot = hwMap;
+        servo2 = hwBot.get(Servo.class, "servo2");          //Control Hub Servo Port 1
+        servo2.setDirection(Servo.Direction.FORWARD);
+    }
+
+
+    public void initServo3(HardwareMap hwMap) {
+        hwBot = hwMap;
+        servo3 = hwBot.get(Servo.class, "servo3");          //Control Hub Servo Port 2
+        servo3.setDirection(Servo.Direction.FORWARD);
+    }
+
+
+    // **** Movement Methods for Fly Wheels ****
+    public void rotateFlyWheel1 (double power) {
+        flyWheel1.setPower(power);
+    }
+
+    public void rotateFlyWheel2 (double power) {
+        flyWheel2.setPower(power);
+    }
+
+    public void stopFlyWheel1 () {
+        flyWheel1.setPower(0);
+    }
+
+    public void stopFlyWheel2 () {
+        flyWheel2.setPower(0);
+    }
+
+    // **** Movement Methods for Worm Gear ****
+    public void wormGearRotateForward (double power) {
+        wormGear.setPower(Math.abs(power));
+    }
+
+    public void wormGearRotateReverse (double power) {
+        wormGear.setPower(-Math.abs(power));
+    }
+
+    public void wormGearStop () {
+        wormGear.setPower(0);
+    }
+
+    // **** Movement Methods for Linear Actuator ****
+    public void extendLinear (double power) {
+        linearActuator.setPower(Math.abs(power));
+    }
+
+    public void retractLinear (double power) {
+        linearActuator.setPower(-Math.abs(power));
+    }
+
+    public void stopLinear () {
+        linearActuator.setPower(0);
+    }
+
+    // **** Movement Methods for Servos ****
+    public void extendServo1() {
+        servo1.setPosition(0.8);
+    }
+    public void extendServo1Partially() {
+        servo1.setPosition(0.5);
+    }
+    public void retractServo1() {
+        servo1.setPosition(0.2);
+    }
+
+    public void extendServo2() {
+        servo2.setPosition(0.8);
+    }
+    public void extendServo2Partially() {
+        servo2.setPosition(0.5);
+    }
+    public void retractServo2() {
+        servo2.setPosition(0.2);
+    }
+
+    public void extendServo3() {
+        servo3.setPosition(0.8);
+    }
+    public void extendServo3Partially() {
+        servo3.setPosition(0.5);
+    }
+    public void retractServo3() {
+        servo3.setPosition(0.2);
+    }
+
+
+
+
+
+
+
+
+}
